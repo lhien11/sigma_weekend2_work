c9820a24921db42b4eabd5923cae1841b0dc1237
Hien Le <lhien11@gmail.com>
2016-11-07 07:17:05 -0600

    first commit

---
+++ server/public/scripts/app.js
@@ -0,0 +1,68 @@
+/*************************** GLOBAL SCOPE **********************************/
+var usersArray = [];
+var currentIndex = 0;
+
+/*************************** MAIN FUNCTION **********************************/
+$(document).ready(function(){
+
+  init();
+  getUserPicture();
+  updateUser(usersArray[3]);
+  //displayItems(usersArray);
+});
+
+/*************************** HELPER FUNCTION **********************************/
+function init(){
+  $.ajax({
+      type: 'GET',
+      url: '/data',
+      async: false,
+      success: function(data){
+          usersArray = data.sigmanauts;
+      },
+      error: function(xhr){
+        console.log('request failed');
+      }
+  })
+}
+
+function displayItems(itemArray){
+  $.each(itemArray, function(i, person){
+    //console.log("index number: ", i);
+    $(".userContainer").append('<div class="person"></div>');
+    var $el = $(".userContainer").children().last();
+    $el.append('<h2>' + person.name + '</h2>');
+    $el.append('<p>' + person.git_username + '</p>');
+    $el.append('<p>' + person.shoutout + '</p>');
+    $el.append('<img src="' + person.avatar + '" />');
+  })
+}
+
+
+function updateUser(person){
+    $('#user-container').fadeOut(function(){
+        $('#user-name').text(person.name);
+        $('#github-username').html(
+            'GitHub: <a href="http://github.com/' + person.git_username + '">' + person.git_username + '</a>'
+        );
+        $('#avatar').attr('src', person.avatar);
+        $('#shoutout').html('"' + person.shoutout + '"');
+        $('#user-container').fadeIn();
+    });
+}
+
+function getUserPicture(){
+    for (var i = 0; i < usersArray.length; i++) {
+        $.ajax({
+            type: 'GET',
+            async: false,
+            url: 'https://api.github.com/users/' + usersArray[i].git_username,
+            success: function(data){
+                usersArray[i].avatar = data.avatar_url;
+            },
+            error: function(){
+              console.log("no image available");
+            }
+        });
+    }
+}
