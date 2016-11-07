911c09a3f3a7568eda550e9e097075d4bbb1f2d7
System Administrator <root@Thaos-MacBook-Air.local>
2016-11-07 09:22:16 -0600

    something

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,15 +1,35 @@
 /*************************** GLOBAL SCOPE **********************************/
 var usersArray = [];
 var currentIndex = 0;
+var count = 0;
 
 /*************************** MAIN FUNCTION **********************************/
 $(document).ready(function(){
 
   init();
   getUserPicture();
-  updateUser(usersArray[3]);
+  //updateUser(usersArray[3]);
   //displayItems(usersArray);
+  //updateUser(usersArray[count]) = setInterval(timer, 500);
+  //updateUser(usersArray[2]);
+  count = usersArray.length-1;
+  var counterIncrement = -1;
+  var counter = setInterval(timer, 5000);
+
+
+  function timer(){
+    count = count + counterIncrement;
+    if(count == 0 || count == usersArray.length-1){
+      counterIncrement = -counterIncrement;
+    }
+    updateUser(usersArray[count]);
+  }
+
+
+
 });
+
+
 
 /*************************** HELPER FUNCTION **********************************/
 function init(){
@@ -29,8 +49,8 @@ function init(){
 function displayItems(itemArray){
   $.each(itemArray, function(i, person){
     //console.log("index number: ", i);
-    $(".userContainer").append('<div class="person"></div>');
-    var $el = $(".userContainer").children().last();
+    $("#user-container").append('<div class="person"></div>');
+    var $el = $("#user-container").children().last();
     $el.append('<h2>' + person.name + '</h2>');
     $el.append('<p>' + person.git_username + '</p>');
     $el.append('<p>' + person.shoutout + '</p>');
---
+++ test.js
@@ -0,0 +1,11 @@
+var count = 10;
+var counterIncrement=-1;
+var counter = setInterval(timer, 500);
+
+function timer() {
+  count = count+counterIncrement;
+  if (count == 0 || count == 10 ) {
+        counterIncrement = -counterIncrement;
+    }
+  console.log(count);
+}
